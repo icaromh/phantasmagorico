9e95c6bb492495c2bdb18d0611b5b5971b336d9a
Ícaro Heimig <icaro@icaromh.com>
2016-06-27 11:54:04 -0300

    Criado exemplo de account.js

---
+++ account.js
@@ -0,0 +1,14 @@
+module.exports = {
+    instagram : {
+        username: 'icaromh',
+        password: 'password do instagram'
+    },
+    slack : {
+        token: 'tokenSlack', // Token da aplicação: https://api.slack.com/methods/chat.postMessage
+        user: 'usuairioSlack' // usuário que vai receber a notificação: https://api.slack.com/methods/chat.postMessage
+    },
+    alelo : {
+        cpf       : '00000000000', // seu cpf em números
+        password  : 'seupassword' // seu password
+    }
+}
